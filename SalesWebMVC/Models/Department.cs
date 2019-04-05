90fac329d78427aca83781610482967f615cab45
Ismael Lucas <maellucas@hotmail.com>
2019-04-05 17:38:37 +0100

    Entities and second migration, implemented seller and total sales

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SalesWebMVC.Models
 {
@@ -9,6 +8,30 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        // Default Constructor
+        public Department()
+        {
+
+        }
+        // Constructor with arguments
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // Add a seller
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        // Calculate number of sales
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DateBirth { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        // Default Constructor
+        public Seller()
+        {
+
+        }
+        // Constructor with arguments
+        public Seller(int id, string name, string email, DateTime dateBirth, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            DateBirth = dateBirth;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        // Custom methods
+        // Add Sale
+        public void AddSales(SalesRecord salesRecord)
+        {
+            Sales.Add(salesRecord);
+        }
+        // Remove Sale
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            Sales.Remove(salesRecord);
+        }
+        // Calculate all Sales from Seller
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(salesRecord => salesRecord.Date >= initial && salesRecord.Date <= final)
+                .Sum(salesRecord => salesRecord.Amount);
+        }
+    }
+}
