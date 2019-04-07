3dc06714547c4c52fcad128a2c8f36660f86f8f5
Ismael Lucas <maellucas@hotmail.com>
2019-04-07 17:50:39 +0100

    SellerServices, Basic FindAll method, Page Sellers created needs edit,details and delete creation

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -6,14 +6,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        // Dependency Injection
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();
+            return View(list);
         }
     }
 }
---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        // Constructor
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        // Find all to return a list of all sellers
+        public List<Seller> FindAll()
+        {
+            // Method to call a database to Sellers and convert to list
+            return _context.Seller.ToList();
+        }
+    }
+}
